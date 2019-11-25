26f8deb775c11cd11aa22abdf1342a1cbde89e94
C. Alex <csiri.om@gmail.com>
2019-11-25 11:48:13 -0600

    MyApp
    Load MyApp

---
+++ MyApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from MyApp import views
+
+urlpatterns = [
+    path('books', views.book_list),
+    path('books/', views.book_list),
+    #path('books/<int:pk>/', views.book_detail),
+]
